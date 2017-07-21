094f0dbce02013f22bfd4001c6112f17398c7945
Jacques Dafflon <jacques.dafflon@gmail.com>
2017-07-21 14:26:07 +0200

    Add util method to remove whitespace

---
+++ dta/util.py
@@ -0,0 +1,17 @@
+from string import whitespace
+
+
+def remove_whitespace(text: str, whitespace_chars: str = whitespace) -> str:
+    """Remove whitespace characters from a string.
+
+    Args:
+        text: The text to remove the whitespace from.
+        whitespace_chars: The whitespace characters to remove from `text` (default: `string.whitespace`).
+
+    Returns: A version of `text` without the characters defined in `whitespace_chars`
+
+    """
+    for char in whitespace_chars:
+        if char in text:
+            text = text.replace(char, '')
+    return text
