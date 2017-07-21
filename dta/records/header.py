a7bbd40fb1716773955fb0a4c6b5cffb56d706c3
Jacques Dafflon <jacques.dafflon@gmail.com>
2017-07-21 13:50:07 +0200

    Update the record's header
    - Add fields validation mixin
    - Use `_template` attribute to generate the headder
    - Add header validation

--- dta/records/header.py
+++ dta/records/header.py
@@ -1,32 +1,62 @@
-from dta import fields
+from datetime import date, timedelta, datetime
+
+from dta.fields import Date, AlphaNumeric, Numeric
+from dta.records.common import FieldsValidationMixin
 from dta.records.record import DTARecord, DTAValueError
 
 
-class DTAHeader(DTARecord):
+class DTAHeader(FieldsValidationMixin):
 
-    processing_date = fields.Date(required=False, default='000000')
-    recipient_clearing_nr = fields.AlphaNumeric(12, required=False)
-    creation_date = fields.Date()
-    client_clearing_nr = fields.AlphaNumeric(7, required=False)
-    sender_id = fields.AlphaNumeric(5)
-    sequence_nr = fields.Numeric(5)
-    transaction_code = fields.Numeric(3)
-    payment_type = fields.Numeric(1, default=0)
+    processing_date = Date(required=False)
+    recipient_clearing = AlphaNumeric(length=12, required=False)
+    creation_date = Date()
+    client_clearing = AlphaNumeric(length=7, required=False)
+    sender_id = AlphaNumeric(length=5)
+    sequence_nr = Numeric(length=5)
+    transaction_type = Numeric(length=3)
+    payment_type = Numeric(length=1, value=0)
+    processing_flag = Numeric(length=1, value=0)
+
+    _template = ('{processing_date}{recipient_clearing}00000'
+                 '{creation_date}{client_clearing}{sender_id}{sequence_nr}{transaction_type}{payment_type}0')
+
+    def __init__(self, processing_date, recipient_clearing, creation_date, client_clearing, sender_id, sequence_nr
+                 ) -> None:
+        super().__init__()
+        self.processing_date = processing_date
+        self.recipient_clearing = recipient_clearing
+        self.creation_date = creation_date
+        self.client_clearing = client_clearing
+        self.sender_id = sender_id
+        self.sequence_nr = sequence_nr
 
     def generate(self):
-        super(DTAHeader, self).generate()
-        return ''.join([
-            self.processing_date,
-            self.recipient_clearing_nr,
-            '00000',  # output sequence number
-            self.creation_date,
-            self.client_clearing_nr,
-            self.sender_id,
-            self.sequence_nr,
-            self.transaction_code,
-            self.payment_type,
-            '0',  # editing flag
-            ])
+        return self._template.format(
+            processing_date=self.processing_date.generate(),
+            recipient_clearing=self.recipient_clearing.generate(),
+            creation_date=self.creation_date.generate(),
+            client_clearing=self.client_clearing.generate(),
+            sender_id=self.sender_id.generate(),
+            sequence_nr=self.sequence_nr.generate(),
+            transaction_type=self.transaction_type.generate(),
+            payment_type=self.payment_type.generate()
+        )
+
+    def validate(self):
+        now = datetime.now()
+        ninety_days_ago = now - timedelta(days=90)
+        ninety_days_ahead = now + timedelta(days=90)
+        if not isinstance(self.creation_date.value, date):
+            format_errors = [f"[{self.__class__.__name__}] INVALID: creation date must contain a valid date."]
+        elif not (ninety_days_ago < self.creation_date < ninety_days_ahead):
+            format_errors = [(f"[{self.__class__.__name__}] INVALID: creation date may not differ by +/- 90 calendar "
+                              f"days 'from the date when read in.")]
+        else:
+            format_errors = []
+
+        # TODO Properly validate bank clearing no. of the client
+
+        return [], [], format_errors
 
     def check(self):
         super(DTAHeader, self).check()
