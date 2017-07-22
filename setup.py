c42b1f7829adaf3f754f4c1d14c69f50f80205f4
Jacques Dafflon <jacques.dafflon@gmail.com>
2017-07-22 02:56:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,29 +5,31 @@ from setuptools import setup, find_packages
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    with open(os.path.join(os.path.dirname(__file__), fname)) as file:
+        return file.read()
 
-setup(name='python-dta',
-    version='0.2',
-    author='Leuchter Open Source Solutions AG',
-    author_email='tryton@leuchterag.ch',
-    url='https://bitbucket.org/loss/python-dta',
-    description='Swiss DTA payment record generator library',
-    long_description=read('README'),
+setup(
+    name='dta',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author='Jacques Dafflon',
+    author_email='jacques.dafflon@gmail.com',
+    url='https://github.com/jacquesd/dta',
+    description='Swiss DTA payment record (TA 836) generator library',
+    long_description=read('README.md'),
     packages=find_packages(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Office/Business :: Financial',
         'Topic :: Office/Business :: Financial :: Accounting',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-    license='GPL-3',
+    license='MIT',
     test_suite='tests',
     zip_safe=False,
     )
