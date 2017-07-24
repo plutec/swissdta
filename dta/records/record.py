c4a06a1589c184ef4c10c6526b42e78d580884d4
Jacques Dafflon <jacques.dafflon@gmail.com>
2017-07-24 16:09:01 +0200

    Record doc

--- dta/records/record.py
+++ dta/records/record.py
@@ -1,3 +1,5 @@
+"""Base class for DTA TA records"""
+
 from itertools import chain
 
 from dta.records.common import ValidationHandler
@@ -5,6 +7,16 @@ from dta.records.header import DTAHeader
 
 
 class DTARecord(ValidationHandler):
+    """Base class for DTA TA records.
+
+    This class should not be instantiated directly but subclassed
+    instead. It automatically generates a empty header required for all
+    types of records.
+
+    The constructor (of this class and its children) should not accept
+    record values. All fields should be set after initialization and
+    all field attributes should use a subclass of `dta.fields.Field`.
+    """
     def __init__(self):
         super().__init__()
         self.header = DTAHeader()
@@ -24,4 +36,11 @@ class DTARecord(ValidationHandler):
         return self.header.has_errors() or super().has_errors()
 
     def validate(self):
+        """Triggers the validation of the record.
+
+        This validate the data in the record according to the
+        validation defined in the `DTA Standards and Formats`_.
+
+        .. _DTA Standards and Formats: https://www.six-interbank-clearing.com/dam/downloads/en/standardization/dta/dta.pdf
+        """
         self.header.validate()
