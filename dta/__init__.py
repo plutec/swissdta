41a35caca2ead86507b078cf20aa2b8621c4218c
Jacques Dafflon <jacques.dafflon@gmail.com>
2017-07-24 16:08:03 +0200

    Package doc

--- dta/__init__.py
+++ dta/__init__.py
@@ -1,3 +1,11 @@
+"""
+.. currentmodule:: dta
+
+
+This package provides the an API to generate DTA record files.
+"""
+
+
 from dta import dta, fields, records
 
 __all__ = ['dta', 'fields', 'records']
