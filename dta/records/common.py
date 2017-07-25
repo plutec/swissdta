66bb8cc5a83ac793c8143d4d468a39355bef6eb5
Jacques Dafflon <jacques.dafflon@gmail.com>
2017-07-25 13:41:30 +0200

    ValidationHandler doc

--- dta/records/common.py
+++ dta/records/common.py
@@ -2,6 +2,11 @@ from collections import defaultdict
 
 
 class ValidationHandler(object):
+    """Mixin class to handle a record's warnings/errors.
+
+    Mixin class to handle the aggregation and flow
+    of warnings and errors for records, header, ...
+    """
 
     def __init__(self, *args, **kwargs):
         self.__validation_warnings = defaultdict(list)
@@ -9,26 +14,62 @@ class ValidationHandler(object):
 
     @property
     def validation_warnings(self):
+        """Return a flat list of all the warnings for all of the fields."""
         return tuple(warning for warnings in self.__validation_warnings.values() for warning in warnings)
 
     @property
     def validation_errors(self):
+        """Return a flat list of all the errors for all of the fields."""
         return tuple(error for errors in self.__validation_errors.values() for error in errors)
 
     def add_warning(self, field_name: str, warning: str):
+        """Add a warning for a specific field.
+
+        Args:
+            field_name: The name of the field to which the warning applies.
+            warning: The warning for the field.
+        """
         self.__validation_warnings[field_name].append(f'[{field_name}] {warning}')
 
     def set_warnings(self, field_name: str, *warnings: str):
+        """Overwrite the warnings for a given field.
+
+        Calling the method without any warnings:
+        ``record.set_warnings(field.name)`` is the
+        recommended method to clear the warnings for a field.
+
+        Args:
+            field_name: The name of the field to overwrite or set the warnings.
+            *warnings: The warnings to set.
+        """
         self.__validation_warnings[field_name] = [f'[{field_name}] {warning}' for warning in warnings]
 
     def add_error(self, field_name: str, error: str):
+        """Add a error for a specific field.
+
+        Args:
+            field_name: The name of the field to which the error applies.
+            error: The warning for the field.
+        """
         self.__validation_errors[field_name].append(f'[{field_name}] {error}')
 
     def set_errors(self, field_name: str, *errors: str):
+        """Overwrite the errors for a given field.
+
+        Calling the method without any errors:
+        ``record.set_errors(field.name)`` is the
+        recommended method to clear the errors for a field.
+
+        Args:
+            field_name: The name of the field to overwrite or set the errors.
+            *errors: The errors to set.
+        """
         self.__validation_errors[field_name] = [f'[{field_name}] {error}' for error in errors]
 
     def has_warnings(self):
+        """Utility method to indicate whether any warnings have been recorded."""
         return any(self.__validation_warnings.values())
 
     def has_errors(self):
+        """Utility method to indicate whether any errors have been recorded."""
         return any(self.__validation_errors.values())
