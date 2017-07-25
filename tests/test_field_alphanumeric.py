0840849956dc0dc2d5d580cad3cfe553b47a2683
Jacques Dafflon <jacques.dafflon@gmail.com>
2017-07-25 16:57:24 +0200

    Lint tests

--- tests/test_field_alphanumeric.py
+++ tests/test_field_alphanumeric.py
@@ -1,4 +1,5 @@
-import pytest
+"""Tests for the Alphanumeric field"""
+import pytest  # pylint: disable=import-error
 
 from dta.fields import AlphaNumeric
 from dta.records.record import DTARecord
@@ -16,7 +17,8 @@ class ANRecord(DTARecord):
     ('Zürich', 'Zuerich'),
     ('Était', 'Etait')
 ))
-def test_characters_conversion(mocker, input_text, expected_text):
+def test_characters_conversion(input_text, expected_text):
+    """Verifies that text is converted correctly"""
     expected_text = expected_text.ljust(10, ' ')
     record = ANRecord()
     record.field = input_text
--- tests/test_record.py
+++ tests/test_record.py
@@ -1,3 +1,4 @@
+"""Tests for the base DTA record"""
 from unittest.mock import patch
 
 from dta.records.record import DTARecord
@@ -10,4 +11,3 @@ def test_validate_header(mocker):
     assert not mocker.called, "header validation should not be called before record validation is triggered"
     record.validate()
     assert mocker.call, "header validation should be called when record validation is triggered"
-
