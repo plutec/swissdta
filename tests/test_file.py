0665780288c937c047e685f2e55a456728ed13be
Jacques Dafflon <jacques.dafflon@gmail.com>
2017-07-26 10:45:24 +0200

    Test reference numbers in dta file

---
+++ tests/test_file.py
@@ -0,0 +1,159 @@
+from datetime import date
+from decimal import Decimal
+from unittest.mock import patch
+
+import pytest
+
+from dta.constants import IdentificationPurpose, ChargesRule
+from dta.dta import DTAFile
+
+
+@pytest.mark.parametrize(('record_data', 'duplicate_record_indexes'), (
+    ([{
+        'reference': '01234567890',
+        'client_account': 'CH38 0888 8123 4567 8901 2',
+        'processing_date': date(2017, 7, 24),
+        'currency': 'CHF',
+        'amount': Decimal(10),
+        'client_address': ('Alphabet Inc', 'Brandschenkestrasse 110', '8002 Zürich'),
+        'recipient_iban': 'CH9300762011623852957',
+        'recipient_name': 'Herr Peter Haller',
+        'recipient_address': ('Marktplaz 4', '9400 Rorschach'),
+        'identification_purpose': IdentificationPurpose.UNSTRUCTURED,
+        'purpose': ('Reference Uniqueness Test', '', ''),
+        'charges_rules': ChargesRule.OUR
+    }, {
+        'reference': '01234567890',
+        'client_account': 'CH38 0888 8123 4567 8901 2',
+        'processing_date': date(2017, 7, 24),
+        'currency': 'CHF',
+        'amount': Decimal(10),
+        'client_address': ('Alphabet Inc', 'Brandschenkestrasse 110', '8002 Zürich'),
+        'recipient_iban': 'CH9300762011623852957',
+        'recipient_name': 'Herr Peter Haller',
+        'recipient_address': ('Marktplaz 4', '9400 Rorschach'),
+        'identification_purpose': IdentificationPurpose.UNSTRUCTURED,
+        'purpose': ('Reference Uniqueness Test', '', ''),
+        'charges_rules': ChargesRule.OUR
+    }], (0, 1)
+    ), ([{
+        'reference': '01234567890',
+        'client_account': 'CH38 0888 8123 4567 8901 2',
+        'processing_date': date(2017, 7, 24),
+        'currency': 'CHF',
+        'amount': Decimal(10),
+        'client_address': ('Alphabet Inc', 'Brandschenkestrasse 110', '8002 Zürich'),
+        'recipient_iban': 'CH9300762011623852957',
+        'recipient_name': 'Herr Peter Haller',
+        'recipient_address': ('Marktplaz 4', '9400 Rorschach'),
+        'identification_purpose': IdentificationPurpose.UNSTRUCTURED,
+        'purpose': ('Reference Uniqueness Test', '', ''),
+        'charges_rules': ChargesRule.OUR
+    }, {
+        'reference': '01234567890',
+        'client_account': 'CH38 0888 8123 4567 8901 2',
+        'processing_date': date(2017, 7, 24),
+        'currency': 'CHF',
+        'amount': Decimal(10),
+        'client_address': ('Alphabet Inc', 'Brandschenkestrasse 110', '8002 Zürich'),
+        'recipient_iban': 'CH9300762011623852957',
+        'recipient_name': 'Herr Peter Haller',
+        'recipient_address': ('Marktplaz 4', '9400 Rorschach'),
+        'identification_purpose': IdentificationPurpose.UNSTRUCTURED,
+        'purpose': ('Reference Uniqueness Test', '', ''),
+        'charges_rules': ChargesRule.OUR
+    }, {
+        'reference': '01234567890',
+        'client_account': 'CH38 0888 8123 4567 8901 2',
+        'processing_date': date(2017, 7, 24),
+        'currency': 'CHF',
+        'amount': Decimal(10),
+        'client_address': ('Alphabet Inc', 'Brandschenkestrasse 110', '8002 Zürich'),
+        'recipient_iban': 'CH9300762011623852957',
+        'recipient_name': 'Herr Peter Haller',
+        'recipient_address': ('Marktplaz 4', '9400 Rorschach'),
+        'identification_purpose': IdentificationPurpose.UNSTRUCTURED,
+        'purpose': ('Reference Uniqueness Test', '', ''),
+        'charges_rules': ChargesRule.OUR
+    }, {
+        'reference': '01234567890',
+        'client_account': 'CH38 0888 8123 4567 8901 2',
+        'processing_date': date(2017, 7, 24),
+        'currency': 'CHF',
+        'amount': Decimal(10),
+        'client_address': ('Alphabet Inc', 'Brandschenkestrasse 110', '8002 Zürich'),
+        'recipient_iban': 'CH9300762011623852957',
+        'recipient_name': 'Herr Peter Haller',
+        'recipient_address': ('Marktplaz 4', '9400 Rorschach'),
+        'identification_purpose': IdentificationPurpose.UNSTRUCTURED,
+        'purpose': ('Reference Uniqueness Test', '', ''),
+        'charges_rules': ChargesRule.OUR
+    }], (0, 1, 2, 3)
+    ), ([{
+        'reference': '01234567890',
+        'client_account': 'CH38 0888 8123 4567 8901 2',
+        'processing_date': date(2017, 7, 24),
+        'currency': 'CHF',
+        'amount': Decimal(10),
+        'client_address': ('Alphabet Inc', 'Brandschenkestrasse 110', '8002 Zürich'),
+        'recipient_iban': 'CH9300762011623852957',
+        'recipient_name': 'Herr Peter Haller',
+        'recipient_address': ('Marktplaz 4', '9400 Rorschach'),
+        'identification_purpose': IdentificationPurpose.UNSTRUCTURED,
+        'purpose': ('Reference Uniqueness Test', '', ''),
+        'charges_rules': ChargesRule.OUR
+    }, {
+        'reference': '01234567890',
+        'client_account': 'CH38 0888 8123 4567 8901 2',
+        'processing_date': date(2017, 7, 24),
+        'currency': 'CHF',
+        'amount': Decimal(10),
+        'client_address': ('Alphabet Inc', 'Brandschenkestrasse 110', '8002 Zürich'),
+        'recipient_iban': 'CH9300762011623852957',
+        'recipient_name': 'Herr Peter Haller',
+        'recipient_address': ('Marktplaz 4', '9400 Rorschach'),
+        'identification_purpose': IdentificationPurpose.UNSTRUCTURED,
+        'purpose': ('Reference Uniqueness Test', '', ''),
+        'charges_rules': ChargesRule.OUR
+    }, {
+        'reference': '01234567891',
+        'client_account': 'CH38 0888 8123 4567 8901 2',
+        'processing_date': date(2017, 7, 24),
+        'currency': 'CHF',
+        'amount': Decimal(10),
+        'client_address': ('Alphabet Inc', 'Brandschenkestrasse 110', '8002 Zürich'),
+        'recipient_iban': 'CH9300762011623852957',
+        'recipient_name': 'Herr Peter Haller',
+        'recipient_address': ('Marktplaz 4', '9400 Rorschach'),
+        'identification_purpose': IdentificationPurpose.UNSTRUCTURED,
+        'purpose': ('Reference Uniqueness Test', '', ''),
+        'charges_rules': ChargesRule.OUR
+    }, {
+        'reference': '01234567890',
+        'client_account': 'CH38 0888 8123 4567 8901 2',
+        'processing_date': date(2017, 7, 24),
+        'currency': 'CHF',
+        'amount': Decimal(10),
+        'client_address': ('Alphabet Inc', 'Brandschenkestrasse 110', '8002 Zürich'),
+        'recipient_iban': 'CH9300762011623852957',
+        'recipient_name': 'Herr Peter Haller',
+        'recipient_address': ('Marktplaz 4', '9400 Rorschach'),
+        'identification_purpose': IdentificationPurpose.UNSTRUCTURED,
+        'purpose': ('Reference Uniqueness Test', '', ''),
+        'charges_rules': ChargesRule.OUR
+    }], (0, 1, 3)
+    )
+))
+def test_references_uniqueness(record_data, duplicate_record_indexes):
+    dta_file = DTAFile(sender_id='ABC12', client_clearing='8888')
+    for record_datum in record_data:
+        dta_file.add_836_record(**record_datum)
+
+    dta_file._sort_records()
+    dta_file._set_sequence_numbers()
+    dta_file.validate()
+
+    for idx in duplicate_record_indexes:
+        record = dta_file.records[idx]
+        assert (f"[reference] DUPLICATE TRANSACTION NUMBER: reference '{record.reference}' is present more than once."
+                in record.validation_errors), f"Reference number '{record.reference}' is not unique within the file."
