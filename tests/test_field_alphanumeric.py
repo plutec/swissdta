0d4de6df6ef80e0df8f16f0c2d0021d70d56218e
Jacques Dafflon <jacques.dafflon@gmail.com>
2017-07-26 10:59:38 +0200

    Test alphanumeric field truncate

--- tests/test_field_alphanumeric.py
+++ tests/test_field_alphanumeric.py
@@ -1,6 +1,7 @@
 """Tests for the Alphanumeric field"""
 import pytest  # pylint: disable=import-error
 
+from dta.constants import FillSide
 from dta.fields import AlphaNumeric
 from dta.records.record import DTARecord
 
@@ -23,3 +24,29 @@ def test_characters_conversion(input_text, expected_text):
     record = ANRecord()
     record.field = input_text
     assert record.field == expected_text
+
+
+@pytest.mark.parametrize(('input_value', 'expected_value', 'expected_warnings', 'expected_errors', 'field_params'), (
+    ('0123', '0123 ', tuple(), tuple(), {'length': 5, 'truncate': True}),
+    ('01234', '01234', tuple(), tuple(), {'length': 5, 'truncate': True}),
+    ('012345', '01234', ("[field] WARNING: '012345' over 5 characters long, truncating to '01234'",), tuple(),
+     {'length': 5, 'truncate': True}),
+    ('0123456789', '01234', ("[field] WARNING: '0123456789' over 5 characters long, truncating to '01234'",),
+     tuple(), {'length': 5, 'truncate': True}),
+    ('0123', '0123 ', tuple(), tuple(), {'length': 5, 'truncate': False}),
+    ('01234', '01234', tuple(), tuple(), {'length': 5, 'truncate': False}),
+    ('012345', '012345', tuple(), ("[field] TOO LONG: '012345' can be at most 5 characters",),
+     {'length': 5, 'truncate': False}),
+    ('0123456789', '0123456789', tuple(),
+     ("[field] TOO LONG: '0123456789' can be at most 5 characters",),
+     {'length': 5, 'truncate': False})
+))
+def test_truncate(input_value, expected_value, expected_warnings, expected_errors, field_params):
+    class TestRecord(DTARecord):
+        field = AlphaNumeric(**field_params)
+
+    record = TestRecord()
+    record.field = input_value
+    assert record.field == expected_value
+    assert record.validation_warnings == expected_warnings
+    assert record.validation_errors == expected_errors
